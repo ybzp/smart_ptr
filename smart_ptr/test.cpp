3ead95cd4a63e726e79252aa44cdf0256a877046
ybzp <150625978+ybzp@users.noreply.github.com>
2024-02-02 22:43:22 +0100

    Changes

--- smart_ptr/test.cpp
+++ smart_ptr/test.cpp
@@ -5,32 +5,33 @@ using std::endl;
 
 struct MyStruct
 {
-	MyStruct() = default;
-	MyStruct(int a, int b) :a(a), b(b) {}
-	int a;
-	int b;
+    MyStruct() = default;
+    MyStruct(int a, int b) :a(a), b(b) {}
+    int a;
+    int b;
 };
+
 
 int main()
 {
-	MyStruct *s = new MyStruct();
-	s->a = 10;
-	s->b = 20;
+    auto s = new MyStruct();
+    s->a = 10;
+    s->b = 20;
 
-	smart_ptr<MyStruct> sp(s);
+    smart_ptr<MyStruct> sp(s);
 
-	cout << sp->a << endl;
-	cout << sp->b << endl;
-	cout << (*sp).a << endl;
+    cout << sp->a << endl;
+    cout << sp->b << endl;
+    cout << (*sp).a << endl;
 
-	auto sp2 = make_smart<MyStruct>(100, 200);
-	cout << sp2->a << endl;
-	cout << sp2->b << endl;
+//    auto sp2 = make_smart<MyStruct>(100, 200);
+//    cout << sp2->a << endl;
+//    cout << sp2->b << endl;
 
-	auto p = sp2.release();
-	cout << p->a << endl;
-	cout << p->b << endl;
-	delete p;
+    auto p = sp.release();
+    cout << p->a << endl;
+    cout << p->b << endl;
+    delete p;
 
-	return 0;
+    return 0;
 }
